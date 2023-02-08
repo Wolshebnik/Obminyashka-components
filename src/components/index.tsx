6e51f681aca18b8f1e1eabaa30cc8beb394d57fa
Voloshkina <101993145+Voloshkina@users.noreply.github.com>
2023-02-08 20:59:06 +0200

    Obmin 280 loader component (#5)
    * created loader
    
    * [OBMIN]-280 created loader
    
    * [OBMIN]-280 added required field
    
    * [OBMIN]-280 added second variant animation
    
    * [OBMIN]-280 fixed styles
    
    * [OBMIN]-280 updated package-lock

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { Avatar } from './avatar';
 export { ButtonTest } from './button-test';
 export { Image } from './image';
+export { Loader } from './loader';
 export * as Icon from './icon';
---
+++ src/components/loader/Loader.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Loader } from '.';
+import { Background } from './styles';
+
+export default {
+  title: 'Loader',
+  component: Loader,
+} as ComponentMeta<typeof Loader>;
+
+const Template: ComponentStory<typeof Loader> = (args) => (
+  <Background>
+    <Loader {...args} />
+  </Background>
+);
+
+export const DefaultLoader = Template.bind({});
+DefaultLoader.args = {
+  styleType: 'default',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  styleType: 'secondary',
+};
