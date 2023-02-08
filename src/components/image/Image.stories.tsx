e9d74dbc50f5e21c92e0fc6b263af51da4f09842
Andrii Teslenko <andrii.teslenko@sweetcode.pro>
2023-02-08 15:57:51 +0200

    [OBMIN]-273 some fix

--- src/components/image/Image.stories.tsx
+++ src/components/image/Image.stories.tsx
@@ -2,6 +2,9 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Image } from '.';
 
+const url =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6RU6qRg_WySsA_1L3g8epabIBD7WejcWTcf70n5Thi9O_aGWnf20ypkJ3len0A4MV9l0&usqp=CAU';
+
 export default {
   title: 'Image',
   component: Image,
@@ -41,24 +44,19 @@ const Template: ComponentStory<typeof Image> = (args) => <Image {...args} />;
 
 export const ImageDefault = Template.bind({});
 ImageDefault.args = {
-  width: 30,
-  height: 28,
-  source:
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6RU6qRg_WySsA_1L3g8epabIBD7WejcWTcf70n5Thi9O_aGWnf20ypkJ3len0A4MV9l0&usqp=CAU',
+  source: url,
 };
 
 export const ImageMiddle = Template.bind({});
 ImageMiddle.args = {
   width: 50,
   height: 50,
-  source:
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6RU6qRg_WySsA_1L3g8epabIBD7WejcWTcf70n5Thi9O_aGWnf20ypkJ3len0A4MV9l0&usqp=CAU',
+  source: url,
 };
 
 export const ImageBig = Template.bind({});
 ImageBig.args = {
   width: 100,
   height: 100,
-  source:
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6RU6qRg_WySsA_1L3g8epabIBD7WejcWTcf70n5Thi9O_aGWnf20ypkJ3len0A4MV9l0&usqp=CAU',
+  source: url,
 };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,4 @@
+export { Avatar } from './avatar';
 export { ButtonTest } from './button-test';
+export { Image } from './image';
 export * as Icon from './icon';
