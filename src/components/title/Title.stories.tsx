9dbabd145fb2c4d2472b01c367c0f5927f677454
Viktoriia Serkiz <v.serkiz@gmail.com>
2023-02-08 20:14:39 +0200

    [OBMIN-271] fixed argTypes

--- src/components/title/Title.stories.tsx
+++ src/components/title/Title.stories.tsx
@@ -1,18 +1,17 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Title } from '.';
+import { argTypes } from './args';
 
 export default {
   title: 'Title',
   component: Title,
-  argTypes: {
-    onClick: { action: 'Click' },
-  },
+  argTypes,
 } as ComponentMeta<typeof Title>;
 
 const Template: ComponentStory<typeof Title> = (args) => <Title {...args} />;
 
 export const TitleDefault = Template.bind({});
 TitleDefault.args = {
-  text: "title",
+  text: 'title',
 };
