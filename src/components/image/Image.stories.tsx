49f7d241cb2ecd6b2eb6812efeb26c51593bbee6
Denys <99205779+Kyslytsia@users.noreply.github.com>
2023-02-08 16:35:09 +0200

    [OBMIN-273] created avatar (#1)
    * [OBMIN-273] created avatar
    
    * [OBMIN]-273 fixed merge conflicts
    
    * [OBMIN]-273  package-lock
    
    * [OBMIN]-273 restore files
    
    * [OBMIN]-273 add storybook argTypes
    
    * [OBMIN]-273 some fix
    
    * [OBMIN]-273 add argTypes file
    
    * [OBMIN]-273 bump version
    
    ---------
    
    Co-authored-by: Andrii Teslenko <andrii.teslenko@sweetcode.pro>

---
+++ src/components/image/Image.stories.tsx
@@ -0,0 +1,34 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Image } from '.';
+import { argTypes } from './argTypes';
+
+const url =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6RU6qRg_WySsA_1L3g8epabIBD7WejcWTcf70n5Thi9O_aGWnf20ypkJ3len0A4MV9l0&usqp=CAU';
+
+export default {
+  title: 'Image',
+  component: Image,
+  argTypes,
+} as ComponentMeta<typeof Image>;
+
+const Template: ComponentStory<typeof Image> = (args) => <Image {...args} />;
+
+export const ImageDefault = Template.bind({});
+ImageDefault.args = {
+  source: url,
+};
+
+export const ImageMiddle = Template.bind({});
+ImageMiddle.args = {
+  width: 50,
+  height: 50,
+  source: url,
+};
+
+export const ImageBig = Template.bind({});
+ImageBig.args = {
+  width: 100,
+  height: 100,
+  source: url,
+};
---
+++ src/components/image/index.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import * as Icon from '../icon';
+import * as Styles from './styles';
+import { ImageProps } from './types';
+
+const Image = ({ width = 30, height = 28, source }: ImageProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  return (
+    <Styles.AvatarBlock
+      style={{
+        height,
+        width,
+      }}
+    >
+      <Styles.Image
+        alt="avatar"
+        src={source}
+        width={width}
+        height={height}
+        isLoading={isLoading}
+        onLoad={() => setIsLoading(false)}
+        onError={() => setIsLoading(false)}
+      />
+
+      {isLoading && <Icon.Loader width={width} height={height} />}
+    </Styles.AvatarBlock>
+  );
+};
+
+export { Image };
