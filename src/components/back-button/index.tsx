f176a76fd4ee11e7da87ea1fd2a1e95525e7e4a1
Voloshkina <101993145+Voloshkina@users.noreply.github.com>
2023-02-08 21:04:11 +0200

    Obmin 267 back button (#2)
    * created loader
    
    * created back button
    
    * [OBMIN]-267 created back button
    
    * [OBMIN]-267 added space
    
    * [OBMIN]-280 created loader
    
    * [OBMIN]-280 added required field
    
    * [OBMIN]-267 fixed styles, added argTypes file
    
    * [OBMIN]-280 added second variant animation
    
    * [OBMIN]-280 fixed styles
    
    * [OBMIN]-280 updated package-lock
    
    ---------
    
    Co-authored-by: Wolshebnik <andteslenko@gmail.com>

---
+++ src/components/back-button/index.tsx
@@ -0,0 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
+import * as Icon from '../icon';
+import * as Styles from './styles';
+import { IBackButton } from './types';
+
+const BackButton = ({ onClick, icon, text, ...props }: IBackButton) => {
+  const navigate = useNavigate();
+
+  return (
+    <Styles.Button onClick={onClick || (() => navigate(-1))} {...props}>
+      <Styles.Icon>{icon || <Icon.ChevronLeft />}</Styles.Icon>
+      {text && <Styles.Text>{text}</Styles.Text>}
+    </Styles.Button>
+  );
+};
+
+export { BackButton };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
+export * as Icon from './icon';
 export { Avatar } from './avatar';
+export { BackButton } from './back-button';
 export { ButtonTest } from './button-test';
 export { Image } from './image';
 export { Loader } from './loader';
-export * as Icon from './icon';
