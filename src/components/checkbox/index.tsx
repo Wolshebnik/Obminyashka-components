ee6519fcb0ceabf13a447d63874a26a08aa744c7
Akintseva Elizaveta <105868447+AkintsevaElizaveta@users.noreply.github.com>
2023-02-12 12:57:19 +0200

    Obmin 266 checkbox (#3)
    * Created component Checkbox for Storybook
    
    * clean up
    
    * Fixed errors
    
    * added space
    
    * added export of the checkbox component
    
    * added onClick
    
    * changed CheckBox component (added input)
    
    * [OBMIN-266] fixed formatting
    
    ---------
    
    Co-authored-by: Andrii Teslenko <andrii.teslenko@sweetcode.pro>

---
+++ src/components/checkbox/index.tsx
@@ -0,0 +1,27 @@
+import * as Icon from '../icon';
+import { IInput } from './types';
+import * as Styles from './styles';
+
+export const CheckBox = ({
+  gap,
+  text,
+  margin,
+  fontSize,
+  onChange,
+  checked = false,
+  type = 'checkbox',
+  ...props
+}: IInput) => (
+  <Styles.Div margin={margin} checked={checked} type={type}>
+    <Styles.Label fontSize={fontSize} checked={checked} gap={gap} type={type}>
+      <Styles.Input
+        type={type}
+        checked={checked}
+        onChange={onChange}
+        {...props}
+      />
+      <span>{text}</span>
+      <Icon.Check />
+    </Styles.Label>
+  </Styles.Div>
+);
--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -3,3 +3,4 @@ export { ReactComponent as ChevronLeft } from 'assets/icons/chevron-left.svg';
 export { ReactComponent as Eye } from 'assets/icons/eye.svg';
 export { ReactComponent as EyeOff } from 'assets/icons/eye-off.svg';
 export { ReactComponent as Loader } from 'assets/icons/loader.svg';
+export { ReactComponent as Check } from 'assets/icons/Check.svg'
