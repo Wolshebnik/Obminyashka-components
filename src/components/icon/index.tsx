54d8beff4dd32d475681a57ab219402a39e32813
Andrii Teslenko <andrii.teslenko@sweetcode.pro>
2023-02-14 21:40:14 +0200

    change name of svg

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,8 +1,8 @@
 export { ReactComponent as Avatar } from 'assets/icons/avatar.svg';
-export { ReactComponent as Check } from "assets/icons/check-checkbox.svg";
+export { ReactComponent as Check } from "assets/icons/check.svg";
 export { ReactComponent as ChevronLeft } from 'assets/icons/chevron-left.svg';
 export { ReactComponent as Eye } from 'assets/icons/eye.svg';
 export { ReactComponent as EyeOff } from 'assets/icons/eye-off.svg';
 export { ReactComponent as Google } from 'assets/icons/google.svg';
 export { ReactComponent as Loader } from 'assets/icons/loader.svg';
-export { ReactComponent as Plus } from 'assets/icons/plus-big.svg';
+export { ReactComponent as Plus } from 'assets/icons/plus.svg';
