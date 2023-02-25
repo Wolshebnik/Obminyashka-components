4a887732209bf45741822e44d976c0b9fc0c2829
Denys <99205779+Kyslytsia@users.noreply.github.com>
2023-02-25 14:51:35 +0200

    Obmin 274 product card (#9)
    * [OBMIN-274] Created product card
    
    * [OBMIN-274] delete commented text
    
    * [OBMIN-274] edded export components, fixed styles
    
    * [OBMIN-274] fixed sort and styles
    
    * [OBMIN-274] fixed sort
    
    * [OBMIN-274] fixed sort
    
    * [OBMIN-274] added some chenges
    
    * [OBMIN-274] added latest chenges
    
    * [OBMIN-274] fixed interface
    
    * [OBMIN-274] Changed version

---
+++ src/components/product-card/arg-types.ts
@@ -0,0 +1,78 @@
+export const argTypes = {
+  city: {
+    name: 'city',
+    type: { name: 'string', required: true },
+    description: 'city name',
+    table: {
+      type: { summary: 'string' },
+    },
+  },
+  text: {
+    name: 'text',
+    type: { name: 'string', required: true },
+    description: 'text',
+    table: {
+      type: { summary: 'string' },
+    },
+  },
+  picture: {
+    name: 'picture',
+    type: { name: 'string', required: true },
+    description: 'src',
+    table: {
+      type: { summary: 'string' },
+    },
+  },
+  buttonText: {
+    name: 'buttonText',
+    type: { name: 'string', required: true },
+    description: 'button text',
+    table: {
+      type: { summary: 'string' },
+    },
+  },
+  isFavorite: {
+    name: 'isFavorite',
+    type: { name: 'boolean' },
+    description: 'isFavorite',
+    table: {
+      type: { summary: 'boolean' },
+    },
+    control: { type: 'boolean' },
+  },
+  onClick: {
+    name: 'onClick',
+    type: { name: 'function' },
+    description: 'Handler click',
+    action: 'Click',
+    table: {
+      type: { summary: 'function' },
+    },
+  },
+  avatar: {
+    name: 'avatar',
+    type: { name: 'string' },
+    description: 'avatar image',
+    table: {
+      type: { summary: 'string' },
+    },
+  },
+  margin: {
+    name: 'margin',
+    type: { name: 'number' },
+    description: 'CSS margin',
+    table: {
+      type: { summary: 'number' },
+      defaultValue: { summary: 0 },
+    },
+  },
+  inboxMessage: {
+    name: 'message',
+    type: { name: 'number' },
+    description: 'inboxMessage',
+    table: {
+      type: { summary: 'number' },
+      defaultValue: { summary: 0 },
+    },
+  },
+};
