037bd0c02bcff357ead3c2dbb5b43d8fcaab67e7
Voloshkina <dp060990voj@gmail.com>
2023-02-27 12:49:35 +0200

    [OBMIN-323] added types to component

---
+++ src/components/product-post-data/index.tsx
@@ -0,0 +1,112 @@
+import { useMemo } from 'react';
+
+import { Button, Icon } from 'components';
+
+import * as Styles from './styles';
+import { IProductPostData } from './types';
+
+const ProductPostData = ({
+  age,
+  lang,
+  size,
+  title,
+  wishes,
+  gender,
+  season,
+  buttonText,
+  readyForOffers,
+  translatedTextAge,
+  translatedTextSize,
+  translatedTextGender,
+  translatedTextSeason,
+  translatedTextCheckInUl,
+  translatedTextChangesTo,
+  translatedTextDescription,
+  ...props
+}: IProductPostData) => {
+  const transformWishes = readyForOffers
+    ? [...(wishes || []), translatedTextCheckInUl]
+    : wishes;
+
+  const sm = useMemo(() => (lang === 'en' ? 'sm' : 'см'), [lang]);
+
+  return (
+    <Styles.Container>
+      <div>
+        <Styles.TitleContainer>
+          <Styles.TitleH2>{title}</Styles.TitleH2>
+        </Styles.TitleContainer>
+
+        <Styles.PostDataDescription>
+          <Styles.PostDataDescriptionText>
+            <Styles.PostDataDescriptionSpan>
+              {`${translatedTextChangesTo}:`}
+            </Styles.PostDataDescriptionSpan>
+          </Styles.PostDataDescriptionText>
+
+          <Styles.PostDataDescriptionOl>
+            {transformWishes?.map((item, idx) => (
+              <Styles.PostDataDescriptionOlItem key={String(`li_${idx}`)}>
+                <Icon.ProductPostCheck style={{ marginRight: '10px' }} />
+                {item}
+              </Styles.PostDataDescriptionOlItem>
+            ))}
+          </Styles.PostDataDescriptionOl>
+        </Styles.PostDataDescription>
+
+        <Styles.ButtonContainer>
+          <Button text={buttonText} width={250} />
+        </Styles.ButtonContainer>
+      </div>
+
+      <div>
+        <Styles.TitleContainer>
+          <Styles.TitleH2>{`${translatedTextDescription}:`}</Styles.TitleH2>
+        </Styles.TitleContainer>
+
+        <Styles.PostDataBoxContainer>
+          <Styles.PostDataDescription>
+            <Styles.PostDataDescriptionUl>
+              <Styles.PostDataDescriptionUlItem>
+                <Styles.PostDataDescriptionSpan>
+                  {`${
+                    size ? `${translatedTextSize} /` : ''
+                  } ${translatedTextAge}:`}
+                </Styles.PostDataDescriptionSpan>
+              </Styles.PostDataDescriptionUlItem>
+
+              <Styles.PostDataDescriptionUlItem>
+                <Styles.PostDataDescriptionSpan>
+                  {`${translatedTextSeason}:`}
+                </Styles.PostDataDescriptionSpan>
+              </Styles.PostDataDescriptionUlItem>
+
+              <Styles.PostDataDescriptionUlItem>
+                <Styles.PostDataDescriptionSpan>
+                  {`${translatedTextGender}:`}
+                </Styles.PostDataDescriptionSpan>
+              </Styles.PostDataDescriptionUlItem>
+            </Styles.PostDataDescriptionUl>
+          </Styles.PostDataDescription>
+
+          <Styles.PostDataDescription>
+            <Styles.PostDataDescriptionUl>
+              <Styles.PostDataDescriptionUlItem>
+                {`${size ? `${size} ${sm} /` : ''} ${age ?? ''}`}
+              </Styles.PostDataDescriptionUlItem>
+
+              <Styles.PostDataDescriptionUlItem>
+                {season}
+              </Styles.PostDataDescriptionUlItem>
+
+              <Styles.PostDataDescriptionUlItem>
+                {gender}
+              </Styles.PostDataDescriptionUlItem>
+            </Styles.PostDataDescriptionUl>
+          </Styles.PostDataDescription>
+        </Styles.PostDataBoxContainer>
+      </div>
+    </Styles.Container>
+  );
+};
+export { ProductPostData };
