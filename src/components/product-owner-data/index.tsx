13b871f0b85f334cbc52c58eeaf534c6fb740224
Denys <99205779+Kyslytsia@users.noreply.github.com>
2023-02-28 22:04:54 +0200

    Obmib 322 create product owner data (#22)
    * [OBMIN-322] Created ProductOwnerData
    
    * [OBMIN-322] Changed props and styles
    
    * [OBMIN-322] Changed props and styles
    
    * [OBMIN-322] Changed props and styles
    
    * [OBMIN-322] Changed arg-types
    
    * [OBMIN-322] some change
    
    * [OBMIN-322] update version

---
+++ src/components/product-owner-data/index.tsx
@@ -0,0 +1,74 @@
+import * as Styles from './styles';
+import { Avatar } from '../avatar';
+import { EllipsisText } from '../ellipsis-text';
+import { IProductOwnerDataProps } from './types';
+
+const ProductOwnerData = ({
+  name,
+  date,
+  city,
+  phone,
+  avatar,
+  dateTranslatedText,
+  cityTranslatedText,
+  phoneTranslatedText,
+}: IProductOwnerDataProps) => {
+  return (
+    <Styles.Container>
+      <Styles.ProductOwner>
+        <Styles.ProductOwnerAvatar>
+          <Avatar height={32} width={32} source={avatar} />
+        </Styles.ProductOwnerAvatar>
+
+        <Styles.ProductOwnerName>
+          <Styles.ProductOwnerTitleH2>{name}</Styles.ProductOwnerTitleH2>
+        </Styles.ProductOwnerName>
+      </Styles.ProductOwner>
+
+      <Styles.PostData>
+        <Styles.PostReqData>
+          <Styles.PostReqDataBlock>
+            {date && (
+              <Styles.PostReqDataText>
+                {dateTranslatedText}
+              </Styles.PostReqDataText>
+            )}
+
+            <Styles.PostReqDataText>
+              {cityTranslatedText}
+            </Styles.PostReqDataText>
+
+            {phone && (
+              <Styles.PostReqDataText>
+                {phoneTranslatedText}
+              </Styles.PostReqDataText>
+            )}
+          </Styles.PostReqDataBlock>
+        </Styles.PostReqData>
+
+        <Styles.PostResData>
+          <Styles.PostReqDataBlock>
+            {date && (
+              <Styles.PostReqDataText color='black'>
+                {date}
+              </Styles.PostReqDataText>
+            )}
+
+            <Styles.PostReqDataText color='black' style={{ width: '200px' }}>
+              <EllipsisText id={city} width={300}>
+                {city}
+              </EllipsisText>
+            </Styles.PostReqDataText>
+
+            {phone && (
+              <Styles.PostReqDataText color='black'>
+                {phone}
+              </Styles.PostReqDataText>
+            )}
+          </Styles.PostReqDataBlock>
+        </Styles.PostResData>
+      </Styles.PostData>
+    </Styles.Container>
+  );
+};
+export { ProductOwnerData };
