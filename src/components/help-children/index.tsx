d617440f4bdad7ec30fd9e9daed94a6edfa68a34
Andrii Teslenko <andteslenko@gmail.com>
2023-03-02 23:34:03 +0200

    relative path

--- src/components/help-children/index.tsx
+++ src/components/help-children/index.tsx
@@ -1,8 +1,7 @@
-import { Button } from 'components/button';
-
 import * as Icon from '../icon';
 import { Title } from '../title';
 import * as Styles from './styles';
+import { Button } from '../button';
 import { IHelpChildren } from './types';
 
 const HelpChildren = ({ name, text, title, buttonText }: IHelpChildren) => {
