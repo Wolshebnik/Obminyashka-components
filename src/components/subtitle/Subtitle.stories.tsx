69128da66e538a5a3cfed76f05948fd9ef916592
Voloshkina <101993145+Voloshkina@users.noreply.github.com>
2023-04-03 12:33:17 +0300

    [OBMIN-349] TitleH3 (#43)
    * [OBMIN-349] created subtitle component
    
    * added font-weight bold
    
    * [OBMIN-349] fixed styles
    
    * [OBMIN-349] fixed styles
    
    * [OBMIN-349] removed variant with another font
    
    * [OBMIN-349] fixed types

---
+++ src/components/subtitle/Subtitle.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Subtitle } from '.';
+import { argTypes } from './arg-types';
+
+export default {
+  title: 'Subtitle',
+  component: Subtitle,
+  argTypes,
+} as ComponentMeta<typeof Subtitle>;
+
+const Template: ComponentStory<typeof Subtitle> = (args) => (
+  <Subtitle {...args} />
+);
+
+export const SubtitleDefault = Template.bind({});
+SubtitleDefault.args = {
+  textTitle: 'Виберiть роздiл',
+};
+
+export const WithoutStar = Template.bind({});
+WithoutStar.args = {
+  hiddenStar: true,
+  textTitle: 'Опис товара',
+};
