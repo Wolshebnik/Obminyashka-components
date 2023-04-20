6b6ba6051545c5d50b6254b0199f5cac99a4b403
Voloshkina <dp060990voj@gmail.com>
2023-04-20 22:02:11 +0300

    [OBMIN-363] fixed import images

--- src/components/search-result/styles.ts
+++ src/components/search-result/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
-import sun from 'assets/img/sun/sun.png';
-import bgCloud from 'assets/img/clouds/bg-cloud.png';
-import sunEmotion from 'assets/img/sun/sun-emotion.png';
+import sun from '../../assets/img/sun/sun.png';
+import bgCloud from '../../assets/img/clouds/bg-cloud.png';
+import sunEmotion from '../../assets/img/sun/sun-emotion.png';
 
 export const Wrapper = styled.div`
   width: 875px;
