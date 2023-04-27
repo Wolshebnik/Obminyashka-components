84d57608a566be16a7d5951ef99140d4e8ecee98
ASGladchenko <asgladchenko@gmail.com>
2023-04-27 20:59:32 +0300

    [OBMIN-374] added React.Fragment

--- src/components/select-lang/index.tsx
+++ src/components/select-lang/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import * as Styles from './styles';
 import { ILanguageSelection } from './types';
 
@@ -6,16 +7,16 @@ const languageArray: string[] = ['ua', 'en'];
 const LanguageSelection = ({ lang, onClick }: ILanguageSelection) => (
   <Styles.LanguagePanel>
     {languageArray.map((el, index) => (
-      <>
-        {!!index && <span key={index + 1}>/</span>}
+      <React.Fragment key={el + index}>
+        {!!index && <span>/</span>}
         <Styles.Language
-          key={el}
+          key={index}
           checked={el === lang}
           onClick={el === lang ? undefined : () => onClick({ lang: el })}
         >
           {el === 'en' ? 'eng' : el}
         </Styles.Language>
-      </>
+      </React.Fragment>
     ))}
   </Styles.LanguagePanel>
 );
