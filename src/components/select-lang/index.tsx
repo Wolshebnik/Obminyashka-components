dcd9436ed8563351f14c28f27f49e8e766f8c2b6
ASGladchenko <105244016+ASGladchenko@users.noreply.github.com>
2023-04-28 16:57:46 +0300

    [OBMIN-374] redesign Language Selection (#48)
    * [OBMIN-374] redesign Language Selection
    
    * [OBMIN-374] redesign Language Selection
    
    * [OBMIN-374] added spacing and change styles
    
    * [OBMIN-374] adaptive select-language, create endpoint to response
    
    * [OBMIN-374] change 'en' for 'eng'
    
    * [OBMIN-374] added React.Fragment
    
    * [OBMIN-374] change style
    
    * [OBMIN-374] added space

--- src/components/select-lang/index.tsx
+++ src/components/select-lang/index.tsx
@@ -1,22 +1,24 @@
+import React from 'react';
 import * as Styles from './styles';
 import { ILanguageSelection } from './types';
 
 const languageArray: string[] = ['ua', 'en'];
 
-const LanguageSelection = ({ lang, onClick }: ILanguageSelection) => {
-  return (
-    <Styles.LanguagePanel>
-      {languageArray.map((el) => (
-        <Styles.LanguageItem
-          key={el}
+const LanguageSelection = ({ lang, onClick }: ILanguageSelection) => (
+  <Styles.LanguagePanel>
+    {languageArray.map((el, index) => (
+      <React.Fragment key={el + index}>
+        {!!index && <span>/</span>}
+        <Styles.Language
+          key={index}
           checked={el === lang}
           onClick={el === lang ? undefined : () => onClick({ lang: el })}
         >
-          {el}
-        </Styles.LanguageItem>
-      ))}
-    </Styles.LanguagePanel>
-  );
-};
+          {el === 'en' ? 'eng' : el}
+        </Styles.Language>
+      </React.Fragment>
+    ))}
+  </Styles.LanguagePanel>
+);
 
 export { LanguageSelection };
