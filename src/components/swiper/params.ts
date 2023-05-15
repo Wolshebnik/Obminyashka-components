65e7c4af12d02d954b00b178a9abf3040af1fa1c
Danylo Hrytsenko <74321926+cloudmenson@users.noreply.github.com>
2023-05-15 10:59:37 +0300

    [Obmin 355] Adaptive slider | Update story-book to 7.0.9 (#58)
    * [OBMIN-355] This commit to make merge a branch with develop
    
    * [OBMIN-355] Updated the Story-book to 7.0.9 version
    
    * [OBMIN-355] Added adaptability to Swiper
    
    * [OBMIN-355] Resized images
    
    * [OBMIN-355] Deleted an unused props and an empty string
    
    * [OBMIN-355] Added direction property for swiper
    
    * [OBMIN-355] Delete direction param

--- src/components/swiper/params.ts
+++ src/components/swiper/params.ts
@@ -3,12 +3,33 @@ import { Autoplay } from 'swiper';
 export const swiperParams = {
   loop: true,
   speed: 2000,
-  spaceBetween: 42,
-  slidesPerView: 4,
+  spaceBetween: 15,
+  slidesPerView: 1,
   modules: [Autoplay],
   className: 'mySwiper',
   autoplay: {
     delay: 4000,
     disableOnInteraction: false,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 28,
+      slidesPerView: 2,
+    },
+
+    1366: {
+      spaceBetween: 25,
+      slidesPerView: 3,
+    },
+
+    1600: {
+      spaceBetween: 30,
+      slidesPerView: 3,
+    },
+
+    1920: {
+      spaceBetween: 42,
+      slidesPerView: 4,
+    },
   },
 };
