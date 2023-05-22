6f3de8a1d7a95f24c4bd6dc27d97e3f2a0566d54
ASGladchenko <asgladchenko@gmail.com>
2023-05-22 22:00:46 +0300

    [OBMIN-408] Change some styles in language select

--- src/components/select-lang/styles.ts
+++ src/components/select-lang/styles.ts
@@ -6,6 +6,27 @@ export const LanguagePanel = styled.div`
   align-items: center;
   gap: 4px;
   width: fit-content;
+  font-family: 'Open Sans', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 22px;
+
+  ${({ theme }) => css`
+    color: ${theme.colors.languageNew.main};
+
+    ${theme.responsive.isTablet &&
+    css`
+      font-size: 22px;
+      line-height: 30px;
+    `}
+
+    ${theme.responsive.isDesktop &&
+    css`
+      font-size: 20px;
+      line-height: 27px;
+    `}
+  `}
 `;
 
 export const Language = styled.button<{ checked: boolean }>`
