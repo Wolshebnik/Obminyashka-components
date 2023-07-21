7548996057081f290e32344e0bf53b9cf7064089
ASGladchenko <asgladchenko@gmail.com>
2023-07-21 16:05:18 +0300

    fixed switcher

--- src/components/burger/helpers.ts
+++ src/components/burger/helpers.ts
@@ -2,9 +2,11 @@ export const positionTop = (size: number) => {
   switch (true) {
     case size <= 768:
       return 0;
+
     case size > 767 && size <= 1366:
       return 85;
-    case size > 1366:
+
+    default:
       return 168;
   }
 };
