b803cdec4e9a9134aa347eab6ca1c8c890d49c43
Denys <99205779+Kyslytsia@users.noreply.github.com>
2024-02-12 09:47:59 +0200

    fix margins (#83)
    * fix margins
    
    * Deleted margin
    
    * delete margin

--- src/components/swiper/styles.ts
+++ src/components/swiper/styles.ts
@@ -4,28 +4,21 @@ import { Link } from 'react-router-dom';
 
 import { IWindowWidth } from './types';
 
-export const SwiperWrapper = styled.div<IWindowWidth>`
-  margin: 50px 0;
+// export const SwiperWrapper = styled.div<IWindowWidth>`
+//   margin: 50px 0;
 
-  ${({ theme, windowWidth }) => css`
-    ${theme.responsive.isTablet &&
-    css`
-      margin: 72px 0;
-    `}
+//   ${({ theme }) => css`
+//     ${theme.responsive.isTablet &&
+//     css`
+//       margin: 70px 0;
+//     `}
 
-    ${theme.responsive.isDesktop &&
-    css`
-      margin: 130px 0;
-    `}
-    
-    ${windowWidth > 1365 &&
-    windowWidth < 1600 &&
-    theme.responsive.isDesktop &&
-    css`
-      margin: 72px 0;
-    `}
-  `}
-`;
+//     ${theme.responsive.isDesktop &&
+//     css`
+//       margin: 130px 0;
+//     `}
+//   `}
+// `;
 
 export const SlideWrapper = styled.div`
   display: flex;
