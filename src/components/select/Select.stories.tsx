aad8a4cd63eb03c692a176f3de248ead33115039
Denys Kyslytsia <kislica779@gmail.com>
2024-04-17 20:44:33 +0300

    Changed Select.stories

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -1,139 +1,31 @@
-/* eslint-disable no-console */
-import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Select } from './index';
-import { IOnChangeValue } from './types';
-import { categoryData, cities, filterData, regions } from './mock';
-
-import * as Styles from './styles';
+import { categoryData } from './mock';
+import { argTypes } from './arg-types';
 
 const meta = {
   title: 'Select',
   component: Select,
+  argTypes,
 } satisfies Meta<typeof Select>;
 
 export default meta;
 type Story = StoryObj<typeof Select>;
 
-const Template = () => {
-  const [open, setOpen] = useState<number>(-1);
-  const [isOpenLocation, setIsOpenLocation] = useState<boolean>(false);
-  const [data, setData] = useState<IOnChangeValue[] | []>([]);
-
-  const setOpenCategory = (id: number) => {
-    if (open === id) {
-      return;
-    }
-
-    setOpen(id);
-  };
-
-  const onChange = (setValues: IOnChangeValue) => {
-    if (!data.length) {
-      setData([setValues]);
-      return;
-    }
-
-    data.map((obj) => {
-      if (obj.value !== setValues.value) {
-        setData([...data, setValues]);
-      }
-
-      if (!setValues.chosenOptions.length) {
-        setData([...data.filter((el) => el.value !== setValues.value)]);
-        return;
-      }
-
-      if (obj.value === setValues.value) {
-        setData([
-          ...data.filter((el) => el.value !== setValues.value),
-          setValues,
-        ]);
-      }
-    });
-  };
-
-  return (
-    <div style={{ width: '300px' }}>
-      <div>
-        <h1>Categories</h1>
-
-        {categoryData.map((el, index) => {
-          return (
-            <Select
-              {...el}
-              disabled={false}
-              onChange={onChange}
-              multiple={el.multiple}
-              key={'category' + index}
-              isActive={open === index}
-              deleteOnClose={el.deleteOnClose}
-              setIsActive={() => setOpenCategory(index)}
-            />
-          );
-        })}
-      </div>
-
-      <div>
-        <h1>Filter</h1>
-
-        <Styles.TitleContainer>
-          <Styles.Title
-            readOnly
-            value={'location'}
-            onClick={() => setIsOpenLocation(!isOpenLocation)}
-          />
-
-          <Styles.Triangle isOpen={isOpenLocation} />
-        </Styles.TitleContainer>
-
-        <Styles.LocationSubCategories filtration isOpen={isOpenLocation}>
-          <Styles.LocationSubCategory filtration>
-            <Select
-              value="99"
-              filtration
-              title="region"
-              options={regions}
-              onChange={(values) => console.log(values)}
-            />
-          </Styles.LocationSubCategory>
-
-          <Styles.LocationSubCategory
-            filtration
-            style={{
-              top: '55px',
-              zIndex: '99',
-            }}
-          >
-            <Select
-              value="100"
-              filtration
-              title="city"
-              options={cities}
-              onChange={(values) => console.log(values)}
-            />
-          </Styles.LocationSubCategory>
-        </Styles.LocationSubCategories>
-
-        {filterData.map((el, index) => {
-          return (
-            <Select
-              {...el}
-              onChange={onChange}
-              key={'filter' + index}
-              multiple={el.multiple}
-              disabled={
-                el.disabled === undefined ? undefined : !(open === el.disabled)
-              }
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
-};
+const options = categoryData[0]?.options || [];
 
 export const SelectCategory: Story = {
-  render: () => <Template />,
+  args: {
+    title: 'clothes',
+    options: options,
+    onChange: () => {},
+  },
+  render: (args) => {
+    return (
+      <div style={{ width: '300px' }}>
+        <Select {...args} />
+      </div>
+    );
+  },
 };
