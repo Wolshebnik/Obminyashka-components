bf610d2d7e66284dfe931c35976d67ca8da7ae95
Denys Kyslytsia <kislica779@gmail.com>
2024-07-24 19:21:35 +0300

    Changed Select

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -25,7 +25,7 @@ export const Select = ({
 }: ISelectProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [chosenOptions, setChosenOptions] = useState<ISelectOption[] | []>(
-    filteredParameterOptions ? filteredParameterOptions : []
+    filteredParameterOptions ?? []
   );
   const [filtrationValue, setFiltrationValue] = useState<string>('');
 
@@ -37,6 +37,9 @@ export const Select = ({
       isEmptyFiltrationValue ||
       el.text.toLowerCase().startsWith(filtrationValue.toLowerCase())
   );
+  const matchesOption = chosenOptions.filter(
+    ({ text }) => text === filtrationValue
+  )[0];
 
   const ref = useRef<HTMLDivElement | null>(null);
 
@@ -70,16 +73,6 @@ export const Select = ({
 
   const onBlur = () => {
     if (filtration) {
-      const matchesOption = chosenOptions.filter(
-        ({ text }) => text === filtrationValue
-      )[0];
-
-      if (matchesOption) {
-        setFiltrationValue(matchesOption.text);
-        setChosenOptions([matchesOption]);
-        onChange({ value: value || '', chosenOptions });
-      }
-
       if (!matchesOption && chosenOptions.length) {
         setFiltrationValue('');
         setChosenOptions([]);
@@ -103,10 +96,38 @@ export const Select = ({
       filteredParameterOptions.length > 0 &&
       !chosenOptions.length
     ) {
-      setOpen();
+      if (!filtration) {
+        setOpen();
+      }
+
       setChosenOptions(filteredParameterOptions);
+
+      if (filtration) {
+        setFiltrationValue(filteredParameterOptions?.[0].text);
+      }
     }
   }, [filteredParameterOptions]);
+
+  useEffect(() => {
+    if (filtration) {
+      if (matchesOption) {
+        setFiltrationValue(matchesOption.text);
+        setChosenOptions([matchesOption]);
+        onChange({
+          value: value ?? '',
+          chosenOptions: chosenOptions,
+        });
+      }
+
+      if (!filtrationValue) {
+        setChosenOptions([]);
+        onChange({
+          value: '',
+          chosenOptions: [],
+        });
+      }
+    }
+  }, [filtrationValue]);
 
   useEffect(() => {
     if ((isOpen || isActive) && !disabled)
